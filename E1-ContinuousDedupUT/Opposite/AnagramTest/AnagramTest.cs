1f6712f0516af8c9b86f8656b01b6c9b13e4ae78
whatifs <50210276+whatifs@users.noreply.github.com>
2019-05-15 23:20:14 +0100

    Add opposite code of experiment (allows duplication)

---
+++ E1-ContinuousDedupUT/Opposite/AnagramTest/AnagramTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using AnagramLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnagramTest
+{
+    [TestClass]
+    public class AnagramTest
+    {
+        [TestMethod]
+        public void IdenticalShortTextIsNotAnAnagram()
+        {
+            Assert.IsFalse(Anagram.IsAnagram("same", "same"));
+        }
+
+        [TestMethod]
+        public void IdenticalLongerTextIsNotAnAnagram()
+        {
+            Assert.IsFalse(Anagram.IsAnagram("same but with spaces", "same but with spaces"));
+        }
+
+        [TestMethod]
+        public void IdenticalTextWithSpacesIsNotAnAnagram()
+        {
+            Assert.IsFalse(Anagram.IsAnagram("same but with spaces", "same but with spaces"));
+        }
+
+        [TestMethod]
+        private static void NullText1IsRejected()
+        {
+            try
+            {
+                Anagram.IsAnagram(null, "valid");
+                Assert.Fail("expected exception not thrown");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.AreEqual("text1", ex.ParamName);
+            }
+        }
+
+        [TestMethod]
+        private static void NullText2IsRejected()
+        {
+            try
+            {
+                Anagram.IsAnagram("valid", null);
+                Assert.Fail("expected exception not thrown");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.AreEqual("text1", ex.ParamName);
+            }
+        }
+
+        [TestMethod]
+        private static void TwoNullTextsAreRejected()
+        {
+            try
+            {
+                Anagram.IsAnagram(null, null);
+                Assert.Fail("expected exception not thrown");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Assert.AreEqual("text1", ex.ParamName);
+            }
+        }
+    }
+}
